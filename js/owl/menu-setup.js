02e06e4672ef8c9f2a221edd53f164ae73327e6a
redpyxel <o.larissax@gmail.com>
2021-04-27 22:49:55 -0300

    new html, css and js files

---
+++ js/owl/menu-setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    items:1,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
---
+++ js/owl/setup.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
+function newtab (link){
+    window.open(link,'_blank');
+}
